8458436346dc2fe1acfbf73e00d106d5c1d6d580
GBaker8806 <baker8806@live.com>
2025-07-11 13:36:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('loadmaster-pro-cache-v1').then(cache => {
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("loadmaster-cache").then(cache => {
       return cache.addAll([
-        './',
-        './index.html',
-        './icon.png',
-        './icon-large.png'
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/modules/vehicle_cg.html"
+        // Add other modules here
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
     })
   );
 });
