e7b573f2fcea3a7222ee2960af33aee76266ecc0
GBaker8806 <baker8806@live.com>
2025-07-12 05:39:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,31 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("loadmaster-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/loadmaster-pro/index.html",
-        "/manifest.json",
-        "/modules/vehicle_cg.html"
-        // Add other modules here
-      ]);
+const CACHE_NAME = "loadmaster-pro-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png",
+  // Add any module pages you want cached offline:
+  "modules/restraint.html",
+  "modules/loadshift.html",
+  "modules/winching.html",
+  "modules/tires_over_100psi.html",
+  "modules/vehiclecg.html",
+  "modules/sleeper.html",
+  "modules/approach.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
